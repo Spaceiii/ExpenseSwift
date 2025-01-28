b548669cf681503d1b4ad51628f6a24625325ddc
Tim M <timothee01meyer@gmail.com>
2025-01-28 09:22:57 +0100

    Initial Commit

---
+++ Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by meyer timothee on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
